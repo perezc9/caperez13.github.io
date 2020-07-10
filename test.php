80f514e57b529a3f77378f46644f2adbec6d5c44
Cesar Perez <perezc9@rpi.edu>
2020-07-09 20:03:05 -0700

    Update test.php

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_REQUEST['name'];
   $email = $_REQUEST['email'];
-  echo "<br />Name: " . "$name" . "<br />Email: " . "$email";
+  echo "<br />Name: " . $name . "<br />Email: " . $email;
 }
 ?>
